bc85c4c527fc777a8ea1862607c235c06507506f
Filip Miskarik <miskfilip@gmail.com>
2022-12-05 18:28:58 +0100

    Day 4

---
+++ code/04a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int start1, end1, start2, end2, count = 0;
+
+    while (scanf("%d-%d,%d-%d", &start1, &end1, &start2, &end2) == 4)
+        if ((start1 >= start2 && end1 <= end2) || (start2 >= start1 && end2 <= end1))
+            count++;
+
+    printf("%d\n", count);
+    return 0;
+}
